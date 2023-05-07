72170033216eb43070025813804f64223bae2679
gala-ch <s_chaparrovilanov22@stud.hwr-berlin.de>
2023-05-07 14:27:58 +0200

    add step 6

--- app/app.py
+++ app/app.py
@@ -1,6 +1,8 @@
 import time
 import redis
 from flask import Flask, render_template
+import pandas as pd
+
 
 app = Flask(__name__)
 cache = redis.Redis(host='redis', port=6379)
@@ -21,5 +23,14 @@ def hello():
     count = get_hit_count()
     return render_template('hello.html', name= "BIPM", count = count)
 
+@app.route('/titanic')
+def titanic():
+    df = pd.read_csv('titanic.csv')
+    table = df.head().to_html(index=False)
+    return render_template('titanic.html', table=table)
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80, debug=True)
+
+
+
